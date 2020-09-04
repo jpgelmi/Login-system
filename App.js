d65598235eef9843c38391dd057ac2327b06c217
Juan Pablo Gelmi <jpgelmi@gmail.com>
2020-09-04 00:24:02 -0400

    LoginForm commit

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
-import React, {useEffect, useState} from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  StatusBar
-} from 'react-native'
+import React,{useEffect, useState} from "react"
+import {SafeAreaView, StatusBar, Text, StyleSheet, View,
+Button, YellowBox} from "react-native"
+import {decode, encode} from "js-base64"
 
-import firebase from "./src/utils/firebase"
-
-import {decode, encode} from "base-64"
-
-import firebase from 'firebase'
-
-if(!global.btoa) global.btoa = encode;
-if(!global.atob) global.atob = decode
-
-import HeaderComponent from "./src/components/Header"
 import Auth from "./src/components/Auth"
 
-export default function App() {
+import firebase from "./src/utils/firebase.js"
+import "firebase/auth";
+import MainComponent from "./src/components/MainComponent"
+
+
+if(!global.btoa) global.btoa = encode;
+if(!global.atob) global.atob = decode;
+
+export default function App(){
+ 
   const [user, setUser] = useState(undefined)
 
   useEffect(() => {
@@ -27,18 +22,20 @@ export default function App() {
       setUser(response)
     })
   })
-
   if(user === undefined) return null
 
-  return (
+  return(
     <>
       <StatusBar barStyle = "light-content"/>
-      <SafeAreaView>
+      <SafeAreaView style = {styles.background}>
         {user ? <MainComponent user = {user}/> : <Auth/>}
       </SafeAreaView>
-      <HeaderComponent/>
     </>
   )
 }
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  background:{
+    //backgroundColor: "#15212b",
+    height: "100%",
+  }
+})
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
     StyleSheet,
     Text,
@@ -6,14 +6,96 @@ import {
     TouchableOpacity,
     TextInput
 } from 'react-native'
-
+import {validateEmail} from "../utils/validations"
+import firebse from "../utils/firebase"
 
 export default function LoginForm(props) {
+    const {changeForm} = props
+
+    const [formData, setFormData] = useState(defaultValue())
+    const [formError, setFormError] = useState({})
+
+    const login = () => {
+        let errors = {}
+
+    if(!formData.mail || !formData.password){
+        if(!formData.mail) errors.mail = true
+        if(!formData.password) errors.password = true
+    }else if(!validateEmail(formData.mail)){
+        errors.mail = true
+    }else{
+        firebase.auth()
+        .signInWithEmailAndPassword(formData.mail, formData.password)
+        .catch(() => {
+            setFormError({
+                mail: true,
+                password: true
+            })
+        })
+    }
+        setFormError(errors)
+    }
+    const onChange = (e, type) => {
+        setFormData({...formData, [type]: e.nativeEvent.text})
+    }
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <>
+            <TextInput
+                style = {[styles.input, formError.mail && styles.error]}
+                placeholder = "Correo electronico"
+                onChange = {(e) => onChange(e, "mail")}
+                placeholderTextColor = "#969696"
+            />
+            <TextInput
+                style ={[styles.input, formError.password && styles.error]}
+                placeholder = "Contraseña"
+                onChange = {(e) => onChange(e, "password")}
+                placeholderTextColor = "#969696"
+                secureTextEntry = {true}
+            />
+            <TouchableOpacity onPress = {login}>
+                <Text style = {styles.btnText}> ingresar</Text>
+            </TouchableOpacity>
+            <View>
+                <TouchableOpacity onPress = {changeForm}>
+                    <Text style = {styles.register}>Registrate</Text>
+                </TouchableOpacity>
+            </View>
+        </>
     )
 }
 
-const styles = StyleSheet.create({})
+function defaultValue(){
+    return{
+        mail: "" ,
+        password: "",
+    }
+}
+
+const styles = StyleSheet.create({
+    btnText:{
+        color: "#fff",
+        fontSize: 18,
+    },
+    input:{
+        height: 50,
+        color: "#fff",
+        width: "80%",
+        marginBottom: 20,
+        backgroundColor: "#1e3040",
+        paddingHorizontal: 20,
+        borderRadius: 50,
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: "#1e3040"
+    },
+    register: {
+        flex:1,
+        justifyContent:"center",
+        marginBottom: 10
+    },
+    error:{
+        borderColor: "#940c0c"
+    }
+})
+
