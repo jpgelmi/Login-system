26ff5ace3fb51f51e98b6875c6ef24af973082fc
Juan Pablo Gelmi <jpgelmi@gmail.com>
2020-09-05 20:49:36 -0400

    Sing out feature

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import Header from "./Header"
+import firebase from "../utils/firebase"
 
 export default function MainComponent(props) {
     const{user} = props
@@ -11,8 +12,26 @@ export default function MainComponent(props) {
         <View>
             <Header/>
             <Text>{user.email}</Text>
+            <View style = {styles.viewCoulse}>
+                {/*Al apretar el botón... Hacemos un LogOut*/}
+                <Text style = {styles.text}
+                onPress ={() => {firebase.auth().signOut()}}>Cerrar sesion</Text>
+            </View>
+
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewCoulse:{
+        backgroundColor: "#820000",
+        borderRadius: 50,
+        paddingVertical:10,
+        paddingHorizontal: 30
+    },
+    text:{
+        fontSize: 16,
+        color: "#fff",
+        textAlign: "center"
+    },
+})
